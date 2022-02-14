73dc4f30db6db4ef02c784a190505146697345e5
Roxana-Costea <99656075+Roxana-Costea@users.noreply.github.com>
2022-02-14 15:42:03 +0200

    Update scale-edl-ip.py

--- scale-edl-ip.py
+++ scale-edl-ip.py
@@ -1,7 +1,11 @@
 import ipaddress
 
+file = open('ip-scale.txt', 'w')
+
 net4 = ipaddress.ip_network('192.0.2.0/21', False)
 for x in net4.hosts():
-    print(x)
+   # print(x)
+    file.write(str(x)+'\n')
 
+file.close()
 
